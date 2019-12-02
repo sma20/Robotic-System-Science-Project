063d1369392dbeabceb84747217f6fb5d22434ca
gregoire-hw <57904342+gregoire-hw@users.noreply.github.com>
2019-12-02 15:58:50 +0000

    Add files via upload

---
+++ rss_project/src/move.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import rospy
+from move_to_goal import movetogoal
+from rss_project.srv import *
+
+def my_callback(request):
+
+    rospy.loginfo("Service called")
+    response=my_goalResponse()
+    move = movetogoal(request.x_goal,request.y_goal)
+    move.moverobot()
+    rospy.loginfo("Finished service")
+    response.success=True
+    return response.success
+
+rospy.init_node('move_to_goal')
+my_service = rospy.Service('/move_robot_to_goal', my_goal , my_callback)
+rospy.loginfo("Service Ready")
+
+
+rospy.spin()
