cb8e0c0fcbb65b48cdf0b9c869b50b4308db0ba3
my-name-is-D <57439775+my-name-is-D@users.noreply.github.com>
2019-12-05 19:24:19 +0000

    Add files via upload
    just added more info

--- rss_project/src/move.py
+++ rss_project/src/move.py
@@ -6,17 +6,19 @@ from rss_project.srv import *
 
 def my_callback(request):
 
-    rospy.loginfo("Service called")
+    rospy.loginfo("move_robot Service called")
     response=my_goalResponse()
+    print("my x and y coordinates")
+    print(request.x_goal,request.y_goal)
     move = movetogoal(request.x_goal,request.y_goal)
     move.moverobot()
-    rospy.loginfo("Finished service")
+    rospy.loginfo("Finished move_robot service")
     response.success=True
     return response.success
 
 rospy.init_node('move_to_goal')
 my_service = rospy.Service('/move_robot_to_goal', my_goal , my_callback)
-rospy.loginfo("Service Ready")
+rospy.loginfo("MOVE_ROBOT Service Ready")
 
 
 rospy.spin()
