f8a35997776f442f3a5f2520915d9b82905335c5
my-name-is-D <57439775+my-name-is-D@users.noreply.github.com>
2019-12-24 13:26:20 +0000

    Update check_final_map.py
    more comments

--- rss_project/src/check_final_map.py
+++ rss_project/src/check_final_map.py
@@ -4,19 +4,20 @@ import rospy
 from work_final_map import get_final_map
 from rss_project.srv import *
 
+#here is called the service to recheck the whole map once map deemed complete 
+
 def service_callback(request): 
-
-
 	rospy.loginfo("check_map Service called")
 	response=find_goalsResponse()
-	start_check=request.start_check
+	start_check=request.start_check #check if the request is True or false to allow service to start
 
-	check= get_final_map(start_check)
-	positionsX, positionsY=check.my_return()
+	check= get_final_map(start_check) #send this order to the function
+	
+	positionsX, positionsY=check.my_return() #get the full trajectory coordinates
 	response.positionsX, response.positionsY= positionsX,positionsY
 	rospy.loginfo("Finished ceck map service")
-	print(response.positionsX, response.positionsY)
-	print(len(response.positionsX))
+	print(response.positionsX, response.positionsY) #check
+	print(len(response.positionsX)) #check
 	request.start_check=False
 	return response.positionsX, response.positionsY
 
